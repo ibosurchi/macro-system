70421ac8f51c5dcd4b4feee2382a8904fa1cc16a
ibosurchi <ibrahimmuzafer00@gmail.com>
2026-08-25 17:50:15 +0300

    Add files via upload

---
+++ login.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+import streamlit as st
+import apex_core as core
+
+core.bootstrap_runtime()
+auth_user = core.render_vip_gate()
+if auth_user and auth_user.get("is_authenticated"):
+    st.switch_page("pages/terminal.py")
